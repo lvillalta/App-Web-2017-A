de89d229842a6f0a35d061a2e5f642cf38ba25e5
lvillalta <luis.villalta@epn.edu.ec>
2017-06-03 08:32:50 -0500

    Function

---
+++ 05-typeScript/TypeScript/Function.js
@@ -0,0 +1,7 @@
+/**
+ * Created by LuisVillalta on 3/6/17.
+ */
+function sumar2Numeros(numero1, numero2) {
+    return numero1 + numero2;
+}
+console.log(sumar2Numeros(1, 2));
---
+++ 05-typeScript/TypeScript/Function.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by LuisVillalta on 3/6/17.
+ */
+function sumar2Numeros (
+    numero1:number,
+    numero2:number
+):number{
+    return numero1+numero2;
+}
+console.log(sumar2Numeros(1,2));
