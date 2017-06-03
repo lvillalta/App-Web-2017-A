91fb6b84652a449d782a0523cc5dd86c8ed19662
lvillalta <luis.villalta@epn.edu.ec>
2017-06-03 09:01:11 -0500

    Empieza-termina

--- 06.-BackEnd/Callbacks/app.js
+++ 06.-BackEnd/Callbacks/app.js
@@ -2,7 +2,10 @@
  * Created by LuisVillalta on 3/6/17.
  */
 const fs = require('fs');
-fs.readFile('texto.txt','utf8', function(err, data) {
+console.log("Empieza");
+fs.readFile('texto.txt','utf8',
+    function(err, data) {
     if (err) throw err;
 console.log(data);
 });
+console.log("Termina");
