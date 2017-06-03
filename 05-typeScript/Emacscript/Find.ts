e3337e2ab4f65273a3a147306cc2037b3b35bb67
lvillalta <luis.villalta@epn.edu.ec>
2017-06-03 07:29:54 -0500

    Find2

--- 05-typeScript/Emacscript/Find.ts
+++ 05-typeScript/Emacscript/Find.ts
@@ -1,3 +1,25 @@
 /**
  * Created by LuisVillalta on 3/6/17.
  */
+let arreglo=[{
+    nombre:"Luis",
+    apellido:"Villalta",
+    id:1
+
+},
+    {
+        nombre:"Juan",
+        apellido:"Rulfo",
+        id:2
+    },
+    {
+        nombre:"Luis",
+        apellido:"Ordoñez",
+        id:3
+    }];
+
+//devuelve el primero que encuentra
+//*devuleve la primera instancia en el arreglo segun un criterio de busqueda
+arreglo.find(function(value){
+    return value.apellido=="Rulfo"
+} );
