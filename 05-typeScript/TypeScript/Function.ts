d434fa6741d93aae61cf33aa7b4a9021af8bf935
lvillalta <luis.villalta@epn.edu.ec>
2017-06-03 08:33:40 -0500

    Function-ParametrosXDefecto

--- 05-typeScript/TypeScript/Function.ts
+++ 05-typeScript/TypeScript/Function.ts
@@ -1,10 +1,11 @@
 /**
  * Created by LuisVillalta on 3/6/17.
  */
+//parametros por defecto
 function sumar2Numeros (
-    numero1:number,
-    numero2:number
+    numero1:number=12,
+    numero2?:number
 ):number{
     return numero1+numero2;
 }
-console.log(sumar2Numeros(1,2));
+console.log(sumar2Numeros());
